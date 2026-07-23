352a337c48c5cc3a59a9779b4613283b88e266bd
A HARSHA VARDHAN REDDY <harsha40407@gmail.com>
2026-07-23 12:12:01 +0530

    Time: 0 ms (100%), Space: 42.3 MB (19.46%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if(num == 1){
+            return true;
+        }
+        int low = 0;
+        int high = num / 2;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            long square = (long) mid*mid;
+            if(square < num){
+                low = mid + 1;
+            }
+            else if(square > num){
+                high = mid - 1;
+            }
+            else{
+                return true;
+            }
+        }
+        return false;
+    }
+}
