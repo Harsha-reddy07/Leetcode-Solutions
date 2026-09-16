c7ae0a4d568353cac3b10448755fafa195ef4163
A HARSHA VARDHAN REDDY <harsha40407@gmail.com>
2026-09-16 21:27:20 +0530

    Time: 0 ms (100%), Space: 43.7 MB (79.5%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+     int rows = matrix.length;
+     int cols = matrix[0].length;
+     int low = 0;
+     int high = rows*cols-1;
+     while(low <= high){
+        int mid = low + (high - low)/2;
+        if(matrix[mid / cols][mid % cols] > target){
+            high = mid - 1;
+        }
+        else if(matrix[mid / cols][mid % cols] < target){
+            low = mid + 1;
+        }
+        else{
+            return true;
+        }
+     } 
+     return false;  
+    }
+}
